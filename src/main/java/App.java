de8fe6ea1ebbbd73158fa739fce2f2672d7c6dd0
soslan <staysaev100398@mail.ru>
2024-04-12 23:57:42 +0300

    Spring project

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,22 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("HelloWorld");
+        System.out.println(bean1.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("HelloWorld");
+
+        System.out.println(bean2.getMessage());
+
+        System.out.println(bean1 == bean2);
+        System.out.println(bean1);
+        System.out.println(bean2);
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessage());
+
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessage());
+        System.out.println(beanCat == beanCat2);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,5 @@
+public class Cat {
+    private String message;
+    public Cat (String message) { this.message = message; }
+    public String getMessage() { return message; }
+}
